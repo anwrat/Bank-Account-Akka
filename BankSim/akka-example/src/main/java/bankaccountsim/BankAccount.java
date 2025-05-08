555e118d9a66d0d8ebdeb7f80d5abe8c5a116a3d
anwrat <anweshrwt22@gmail.com>
2025-05-08 21:24:44 +0545

    Main class creating an instance of BankAccount and a start method in BankAccount

--- BankSim/akka-example/src/main/java/bankaccountsim/BankAccount.java
+++ BankSim/akka-example/src/main/java/bankaccountsim/BankAccount.java
@@ -1,5 +1,23 @@
 package bankaccountsim;
 
-public class BankAccount {
+import akka.actor.AbstractActor;
 
+public class BankAccount extends AbstractActor {
+	
+	private double balance;
+    @Override 
+    public Receive createReceive() {  // This method is called when  message is sent
+        return receiveBuilder()
+                 // Matching the message 
+        		.matchEquals("Start", msg->{startup();})
+                .matchAny(msg -> {System.out.println("BankAccount recieved message "+ msg); // Handle any other message
+                })
+                .build(); // Build is done only once.
+    }
+    
+    //Method to initialize balance and display to output
+    public void startup() {
+    	System.out.println("Hello and Welcome to the Bank Simulation");
+    	balance=100;
+    }
 }
--- BankSim/akka-example/src/main/java/bankaccountsim/Main.java
+++ BankSim/akka-example/src/main/java/bankaccountsim/Main.java
@@ -1,5 +1,15 @@
 package bankaccountsim;
 
-public class Main {
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
 
+public class Main {
+	public static void main(String[] args) {
+		ActorSystem system = ActorSystem.create();
+		//Creating an actor of BankAccount class
+		ActorRef bankaccount = system.actorOf(Props.create(BankAccount.class));
+		//BankAccount actor sending message to itself
+		bankaccount.tell("Start", bankaccount);
+	}
 }
