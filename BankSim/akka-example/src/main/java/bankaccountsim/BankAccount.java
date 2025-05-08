129335be3e733c5d9789bb411544b02b2a9d54f8
anwrat <anweshrwt22@gmail.com>
2025-05-08 22:22:07 +0545

    Added logic to check balance before withdrawing and end of program

--- BankSim/akka-example/src/main/java/bankaccountsim/BankAccount.java
+++ BankSim/akka-example/src/main/java/bankaccountsim/BankAccount.java
@@ -5,6 +5,7 @@ import akka.actor.AbstractActor;
 public class BankAccount extends AbstractActor {
 	
 	private double balance;
+	private int transactionCount = 0;
     @Override 
     public Receive createReceive() {  // This method is called when  message is sent
         return receiveBuilder()
@@ -13,14 +14,23 @@ public class BankAccount extends AbstractActor {
         		.match(Deposit.class, deposit->{
         			System.out.println("The amount generated is £ "+deposit.getAmount());
         			balance += deposit.getAmount();
-        			System.out.println("Deposited Amount £ " + deposit.getAmount() + ". New Balance: " +balance);
+        			System.out.println("Deposited Amount £ " + deposit.getAmount() + ". New Balance: £" +balance);
         	    	System.out.println("-------------------");
+        	    	checkIfFinished();
         		})
         		.match(Withdrawal.class, withdrawal->{
-        			System.out.println("The amount generated is £ "+withdrawal.getAmount());
-        			balance += withdrawal.getAmount();
-        			System.out.println("Withdrawn Amount £ " + withdrawal.getAmount() + ". New Balance: " +balance);
-        	    	System.out.println("-------------------");
+        			//Converting the balance to absolute
+        			System.out.println("The amount generated is £ "+ Math.abs(withdrawal.getAmount()));
+        			if(balance>=Math.abs(withdrawal.getAmount())) {
+        				balance -= Math.abs(withdrawal.getAmount());
+        				System.out.println("Withdrawn Amount £ " + Math.abs(withdrawal.getAmount()) + ". New Balance: £" +balance);
+        				System.out.println("-------------------");        				
+        			}
+        			else {
+        				System.out.println("Insufficient funds. Withdrawal amount of £ " + Math.abs(withdrawal.getAmount()) + " denied.");
+        				System.out.println("-------------------");  
+        			}
+        			checkIfFinished();
         		})
                 .build(); // Build is done only once.
     }
@@ -32,5 +42,14 @@ public class BankAccount extends AbstractActor {
     	System.out.println("Your current balance is £ " + balance);
     	System.out.println("-------------------");
     	System.out.println("-------------------");
+    }
+    
+    //Method to check end of program
+    private void checkIfFinished() {
+    	transactionCount++;
+    	if (transactionCount == 10) {
+    		System.out.println("End of program!!!!!!!");
+    		getContext().getSystem().terminate(); 
+    	}
     }
 }
